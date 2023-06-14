85e25c0dfe6992475cce969da1ee4bef0f8ff79a
hnquy156 <hnquy.hbt@gmail.com>
2023-06-14 20:00:38 +0700

    Add http errors

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Response } from 'express';
+import { Unauthorized } from 'http-errors';
 import JWTUtils from '../utils/jwt';
 import { CustomRequest } from '../interfaces/express';
 import UserModel from '../models/users';
@@ -9,17 +10,21 @@ class AuthMiddleware {
     res: Response,
     next: NextFunction
   ) {
-    const token = req.headers.authorization as string;
-    const { userId } = JWTUtils.verifyToken(token);
+    try {
+      const token = req.headers.authorization as string;
+      const { userId } = JWTUtils.verifyToken(token);
 
-    const user = await UserModel.findById(userId);
-    if (!user || user.token !== token) {
-      throw 'Token is invalid!';
+      const user = await UserModel.findById(userId);
+      if (!user || user.token !== token) {
+        throw Unauthorized('Token is invalid!');
+      }
+
+      req.user = { userId };
+
+      next();
+    } catch (error: any) {
+      next(Unauthorized(error.message));
     }
-
-    req.user = { userId };
-
-    next();
   }
 }
 
